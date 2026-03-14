b7abe546ff5126eba7b4c4850695cb9d42028fb3
jussitus <>
2026-03-15 00:11:02 +0200

    add delaunay condition test

---
+++ src/condition.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def ccw(a,b,c):
+    return np.linalg.det(
+        [[a[0], a[1], 1],
+        [b[0], b[1], 1],
+        [c[0], c[1], 1]]) > 0
+
+def inCircle(a,b,c,d):
+    return np.linalg.det(
+        [[a[0], a[1], (a[0])**2 + (a[1])**2,  1],
+        [b[0], b[1], (b[0])**2 + (b[1])**2, 1],
+        [c[0], c[1], (c[0])**2 + (c[1])**2, 1],
+        [d[0], d[1], (d[0])**2 + (d[1])**2, 1]]
+        ) > 0
+
--- src/test.py
+++ src/test.py
@@ -1,8 +1,14 @@
-from edge import Edge, makeQuadEdge, splice, connect
-
+from edge import Edge, makeQuadEdge, splice, connect, deleteEdge
+from condition import ccw, inCircle
 a = makeQuadEdge(1,2)
 b = makeQuadEdge(3,4)
-c = connect(a,b)
-print(a.org,a.dest)
-print(c.org, c.dest)
-print(b.org,b.dest)
+print(a.onext.org, b.onext.org)
+splice(a,b)
+print(a.onext.org, b.onext.org)
+
+a = (1,1)
+b = (0,1)
+c = (0,-1)
+d = (1,1)
+print(ccw(b,a,d))
+print(inCircle(a,b,c,d))
