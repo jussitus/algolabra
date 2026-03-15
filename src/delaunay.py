f9d13247d948951c05b5f10ecba1ac49c42239c2
jussitus <>
2026-03-15 22:40:08 +0200

    edge list

--- src/delaunay.py
+++ src/delaunay.py
@@ -1,27 +1,30 @@
 from edge import Edge, makeQuadEdge, splice, connect, deleteEdge
 from condition import ccw, inCircle, rightOf, leftOf, valid
 
+
 def delaunay(s) -> (Edge, Edge):
     # s must be sorted in increasing x direction
+    if len(s) < 2:
+        raise ValueError(f"len(s)={len(s)} is less than 2")
     if len(s) == 2:
-        a = makeQuadEdge(s[0],s[1])
+        a = makeQuadEdge(s[0], s[1])
         return (a, a.sym)
     elif len(s) == 3:
-        a = makeQuadEdge(s[0],s[1])
-        b = makeQuadEdge(s[1],s[2])
-        splice(a.sym,b)
-        if ccw(s[0],s[1],s[2]):
-            c = connect(b,a)
-            return (a,b.sym)
-        elif ccw(s[0],s[2],s[1]):
-            c = connect(b,a)
+        a = makeQuadEdge(s[0], s[1])
+        b = makeQuadEdge(s[1], s[2])
+        splice(a.sym, b)
+        if ccw(s[0], s[1], s[2]):
+            c = connect(b, a)
+            return (a, b.sym)
+        elif ccw(s[0], s[2], s[1]):
+            c = connect(b, a)
             return (c.sym, c)
         else:
             return (a, b.sym)
 
     else:
         mid = len(s) // 2
-        (ldo,ldi) = delaunay(s[:mid])
+        (ldo, ldi) = delaunay(s[:mid])
         (rdi, rdo) = delaunay(s[mid:])
 
         while True:
@@ -40,24 +43,27 @@ def delaunay(s) -> (Edge, Edge):
         # merge loop
         while True:
             lcand = basel.sym.onext
-            if valid(lcand,basel):
-                while inCircle(basel.dest,basel.org,lcand.dest,lcand.onext.dest):
+            if valid(lcand, basel):
+                while inCircle(basel.dest, basel.org, lcand.dest, lcand.onext.dest):
                     t = lcand.onext
                     deleteEdge(lcand)
                     lcand = t
             rcand = basel.oprev
 
-            if valid(rcand,basel):
-                while inCircle(basel.dest,basel.org,rcand.dest,rcand.oprev.dest):
+            if valid(rcand, basel):
+                while inCircle(basel.dest, basel.org, rcand.dest, rcand.oprev.dest):
                     t = rcand.oprev
                     deleteEdge(rcand)
                     rcand = t
 
-            if not valid(lcand,basel) and not valid(rcand,basel):
+            if not valid(lcand, basel) and not valid(rcand, basel):
                 break
 
-            if not valid(lcand,basel) or (valid(rcand,basel) and inCircle(lcand.dest, lcand.org, rcand.org, rcand.dest)):
-                basel = connect(rcand,basel.sym)
+            if not valid(lcand, basel) or (
+                valid(rcand, basel)
+                and inCircle(lcand.dest, lcand.org, rcand.org, rcand.dest)
+            ):
+                basel = connect(rcand, basel.sym)
             else:
                 basel = connect(basel.sym, lcand.sym)
-        return (ldo,rdo)
+    return (ldo, rdo)
--- src/test.py
+++ src/test.py
@@ -2,31 +2,37 @@ from delaunay import delaunay
 import random
 import networkx as nx
 import matplotlib.pyplot as plt
-s = set()
-random.seed(42)
-for i in range(0,25):
-    s.add((random.randint(0,25),random.randint(0,25)))
-s = sorted(s)
-#print(s)
+from search import bfs_edges
+from point_generation import points_spread, points_random
+
+s = points_random(25, 250, 250, 42)
 
 
-l,r = delaunay(s)
-
-es = []
-for i in range(100):
-    es.append(l)
-    l = l.lnext
+l, r = delaunay(s)
+edges = bfs_edges(l)
+# for e in edges:
+#    print(e)
+first = l
+current = l.rprev
+edges.remove(first)
+while first != current:
+    print(current)
+    # edges.remove(current)
+    # edges.remove(current.sym)
+    current = current.rprev
 
 
 G = nx.Graph()
 
-for e in es:
+for e in edges:
     a = e.org
     b = e.dest
-    G.add_edge(a,b)
+    G.add_edge(a, b)
 
-for e in G.edges():
-    print(e)
+# for e in G.edges():
+# print(e)
 pos = {node: node for node in G.nodes()}
-nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=5)
+nx.draw(G, pos, with_labels=False, node_color="lightblue", node_size=5)
 plt.show()
+
+print(s)
