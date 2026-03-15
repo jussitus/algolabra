73e6480c27166c7d579f3a66d5be4e45de273804
jussitus <>
2026-03-15 04:13:18 +0200

    graphing

--- src/test.py
+++ src/test.py
@@ -1,15 +1,32 @@
 from delaunay import delaunay
 import random
-
+import networkx as nx
+import matplotlib.pyplot as plt
 s = set()
 random.seed(42)
-for i in range(0,1000):
-    s.add((random.randint(0,200),random.randint(0,200)))
+for i in range(0,25):
+    s.add((random.randint(0,25),random.randint(0,25)))
 s = sorted(s)
 #print(s)
 
-d = delaunay(s)[0]
-print(d)
-for i in range(1,10):
-    d = d.lnext
-    print(f'lnext {i}: {d}')
+
+l,r = delaunay(s)
+
+es = []
+for i in range(100):
+    es.append(l)
+    l = l.lnext
+
+
+G = nx.Graph()
+
+for e in es:
+    a = e.org
+    b = e.dest
+    G.add_edge(a,b)
+
+for e in G.edges():
+    print(e)
+pos = {node: node for node in G.nodes()}
+nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=5)
+plt.show()
